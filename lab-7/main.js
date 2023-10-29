ff41728b59fa07612b11ce72d0148829f8096ab7
MemmedovaEsmira <memmedovaesmira151@gmail.com>
2023-10-29 21:36:27 +0400

    lab 7 added

---
+++ lab-7/main.js
@@ -0,0 +1,32 @@
+// let getCountry = prompt("Pleace enter country");
+// let capital;
+//    switch (getCountry) {
+//        case "AZERBAIJAN":
+//        alert("BAKU");
+//        break;
+
+//        case "ITALY":
+//        alert("ROME");
+//        break;
+
+//        case "Czech":
+//        alert("Prague");
+//        break;
+
+//        default:
+//        alert("Data is not defined");
+//    } 
+
+
+// const getSecond =parseInt(prompt("Enter a second"));
+// let minute = Math.floor(getSecond / 60);
+// let second = getSecond %60;
+// alert(minute + "minute" + " " + second +"second");
+
+
+let getNumber = parseInt(prompt("Enter number"));
+let factorial = 1;
+for (let i = 1; i<= getNumber; i++) {
+   factorial = factorial *(getNumber-1);   
+}
+alert(factorial);
