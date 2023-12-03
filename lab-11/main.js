487afe20ba75be68255084d7e63a16c0776a58c8
MemmedovaEsmira <memmedovaesmira151@gmail.com>
2023-12-03 20:48:46 +0400

    lab added

---
+++ lab-11/main.js
@@ -0,0 +1,34 @@
+let toDoEntryButton = document.querySelector(".toDo_entry_button");
+let toDoEntryInput = document.querySelector(".toDo_entry_input");
+let toDoPlanList = document.querySelector(".toDo_plan_list");  
+
+toDoEntryButton.addEventListener("click", () => {
+    let toDoPlanListItem = document.createElement("Li");
+    toDoPlanListItem.innerText = toDoEntryInput.value.trim(); 
+    toDoPlanListItem.classList.add("toDoPlanListItem");
+
+    let toDoPlanRemoveButton = document.createElement("p");
+    toDoPlanRemoveButton.innerText = "x"; 
+    toDoPlanRemoveButton.classList.add("toDoPlanRemoveButton");
+
+    let toDoPlanListWrapper = document.createElement("div");
+    toDoPlanListWrapper.classList.add("toDoPlanListWrapper");
+
+    toDoPlanListWrapper.appendChild(toDoPlanListItem);
+    toDoPlanListWrapper.appendChild(toDoPlanRemoveButton)
+
+    if (toDoEntryInput.value.length > 0) {
+        toDoPlanList.appendChild(toDoPlanListWrapper);
+    }
+    else{
+        alert("please write anything");
+    }
+
+    toDoPlanRemoveButton.addEventListener("click", ()=>{
+        toDoPlanListWrapper.remove()
+    })
+    toDoEntryInput.value = ""
+});
+
+
+
