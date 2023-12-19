daf1606bb2bc45f06c01ae97a1f6c60403ee5c1a
MemmedovaEsmira <memmedovaesmira151@gmail.com>
2023-12-19 20:33:06 +0400

    lab_13_result added

--- lab_13/main.js
+++ lab_13/main.js
@@ -1,28 +1,41 @@
-const url =document.querySelector("")
-const key = document.querySelector("")
-const card_input = document.querySelector("card_input")
+const key = "87bce6a0a01f1e5908ee954bb733c902";
+const input = document.querySelector(".card_input");
 
-input.addEventListener("keypress" ,getCardInputValue);
+input.addEventListener("keypress", getCardInputValue);
 function getCardInputValue(e) {
-    if (e.keyCode=="13") {
-        getCardData(card_input.valiu);
+    if (e.keyCode == "13") {
+        getCardData(input.value);
     }
-    console.log(getCardInputValue)
 }
 function getCardData(cardName) {
-    const requestUrl=`  `
+    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cardName}&appid=${key}&units=metric&lang=az;`
     fetch(requestUrl)
-    .then ((responsive) => responsive.json())
+    .then ((response) => response.json())
     .then((data) =>resultData(data));
 }
 
 function resultData(result) {
     let cardName = document.querySelector(".card_name");
-    cardName.innerText = ${result.name} {result.sys.country}
+    cardName.innerText = `${result.name} ${result.sys.country}`;
 
     let cardTemp = document.querySelector(".card_temp");
-    cardTemp.innerText = ${Math.round(result.main.temp)} °C
+    cardTemp.innerText = `${Math.round(result.main.temp)} °C`;
    
     let cardDesc = document.querySelector(".card_desc");
-    cardDesc.innerText = ${result.wather[0].description}
+    cardDesc.innerText = `${result.wather[0].description}`;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
